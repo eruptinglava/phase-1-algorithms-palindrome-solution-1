0959658c39a9406b7552ddf01ad0146a6902603d
Lava Ahmed <lavaahmed915@gmail.com>
2024-05-14 19:03:30 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = word.split("").reverse().join("");
+  return word === reverseWord;
 }
 
 /* 
   Add your pseudocode here
+  if the word is the same as the word in reverse return true,
+
+  if the input the same as the word in reverse
+  return true
+  else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  I take the input then convert it as array of letters and reverse it then return it to string,
+  if the word is the same as the word in reverse return true else return false,
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
